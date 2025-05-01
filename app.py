324fa482d464d4fb5f9523fb3855168ffe7bf5c8
Jeth Masaya <jethrixmasaya@gmail.com>
2025-05-01 21:32:27 +0800

    update

--- app.py
+++ app.py
@@ -90,5 +90,6 @@ def update_password():
         flash("No password provided.")
     return redirect(url_for("index"))
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
